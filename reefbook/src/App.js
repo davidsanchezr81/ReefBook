76dbeff386e70b56c8e6138c0507e19c02f718d2
David Sanchez Rodriguez <davidsanchezrodriguez@Davids-MacBook-Pro.local>
2020-08-31 20:12:05 +0100

    Transition, Images and Button added to slider

--- reefbook/src/App.js
+++ reefbook/src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import Main from './components/Main'
 import Header from './components/Header'
 import Content from './components/Content'
+import Slider from './components/Slider';
 // import logo from './logo.svg';
 // import './App.css';
 
-class App extends Component {
+
+const images = [
+    'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+    'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+    'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+    'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
+  ]
+  
+ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { apiResponse: "" };
@@ -25,10 +34,11 @@ class App extends Component {
         return (
             
             <div>
-                <Header
+                {/* <Header
                     name="David"
                 />
-                <div><Content /></div>
+                <div><Content /></div> */}
+                <div><Slider  slides={images} /></div>
             </div>
         );
     }
