cc052d50591127ffe501ee66dd8c5fbb7cfc9e51
David Sanchez Rodriguez <davidsanchezr81@gmail.com>
2020-10-18 20:27:55 +0100

    enviromental variables created for DB_connect

--- reefbookservices/server.js
+++ reefbookservices/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, 
+{ userNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log(`connection to database established`)
+}).catch(err=>{
+    console.log(`db error ${err.message}`);
+    process.exit(-1)
+});
 
 //Import Routes
 const authRoute = require('./routes/auth');
