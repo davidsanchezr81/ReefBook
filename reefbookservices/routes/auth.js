544fd686316018ccbc8f57ea672a9defe513b795
David Sanchez Rodriguez <davidsanchezr81@gmail.com>
2020-10-19 17:11:09 +0100

    Data validation added to the code for inputing data into DB

--- reefbookservices/routes/auth.js
+++ reefbookservices/routes/auth.js
@@ -1,15 +1,27 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation} = require('../validation')
 
 router.post('/register' , async (req, res) => {
-    const user = new User({
+
+    //Validation before creating a user
+   const { error } = registerValidation(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+
+    //Check if the user already exist in the database
+    const emailExist = await User.findOne({email : req.body.email})
+    if(emailExist) return res.status(400).send('Email alrady exists');
+
+   //Create a new user
+   const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    });
-    try{
+   });
+   try{
         const savedUser = await user.save();
         res.send(savedUser);
+   
     }catch(err)
     {
         res.status(400).send(err);
